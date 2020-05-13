91752fddcb8edcb46868e3d5c65ada2f67aaca3a
manchandajayant <maanjayant@gmail.com>
2020-05-13 10:45:59 +0200

    refetch

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -6,17 +6,21 @@ const JokesFetched = () => {
   const [punch, setPunch] = useState("");
   const [setup, setSetup] = useState("");
   const [ready, setReady] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  const fetchData = async () => {
+    const result = await axios(
+      "https://official-joke-api.appspot.com/jokes/programming/random"
+    );
+    setData(result.data[0]);
+    setSetup(result.data[0].setup);
+    setTimeout(() => {
+      setReady(true);
+    }, 3000);
+    setLoading(false);
+  };
+
   useEffect(() => {
-    const fetchData = async () => {
-      const result = await axios(
-        "https://official-joke-api.appspot.com/jokes/programming/random"
-      );
-      setData(result.data[0]);
-      setSetup(result.data[0].setup);
-      setTimeout(() => {
-        setReady(true);
-      }, 3000);
-    };
     fetchData();
   }, []);
 
@@ -24,13 +28,24 @@ const JokesFetched = () => {
     setPunch(data.punchline);
   }, [ready]);
 
-  if (!data) {
+  const reFetch = () => {
+    setData([]);
+    setSetup("");
+    setPunch("");
+    setReady(false);
+    setLoading(true);
+    fetchData();
+    console.log("data", data);
+  };
+
+  if (loading) {
     return <h1>Loading....</h1>;
   } else {
     return (
       <div>
         <p>{setup}</p>
         <p>{punch}</p>
+        <button onClick={reFetch}>click</button>
       </div>
     );
   }
